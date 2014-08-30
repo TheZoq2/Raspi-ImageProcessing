4107d32eaa4c57b4dc6fdd716fae93223e0e3a74
frans <frans@frans-ultrabook.lan>
2014-08-30 12:13:55 +0200

    Restructuref file tree to contain desktop version

---
+++ desktop/build/CMakeFiles/tracking.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/main.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/main.cpp.o"
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/objectTracking/BinMap.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/objectTracking/BinMap.cpp.o"
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/objectTracking/ColorTracker.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/objectTracking/ColorTracker.cpp.o"
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/objectTracking/Flooder.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/objectTracking/Flooder.cpp.o"
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/objectTracking/ImgFunc.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/objectTracking/ImgFunc.cpp.o"
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/objectTracking/Vec2.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/objectTracking/Vec2.cpp.o"
+  "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/objectTracking/Vec3.cpp" "/home/frans/Documents/cpp/openCV/Raspi-ImageProcessing/desktop/build/CMakeFiles/tracking.dir/objectTracking/Vec3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ desktop/main.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <time.h>
+#include <unistd.h>
+
+#include "objectTracking/ColorTracker.h"
+#include "objectTracking/Flooder.h"
+
+Vec3 targetColor(29, 227, 218);
+Vec3 minColor;
+Vec3 maxColor;
+Vec3 redColor(170, 227, 218);
+Vec3 minRed;
+Vec3 maxRed;
+
+Vec3 threshold(10, 100, 100);
+
+int main()
+{
+    cv::VideoCapture camera(0);
+
+    if(camera.isOpened() == false)
+    {
+        std::cout << "Failed to open camera" << std::endl;
+    }
+
+    //capture 1 image for use as reference
+    cv::Mat ref;
+    camera >> ref;
+
+    ColorTracker ct;
+    ct.setImage(ref);
+
+    //targetColor = ct.getObjectColor();
+    minColor = targetColor - threshold;
+    maxColor = targetColor + threshold;
+    minRed = redColor - threshold;
+    maxRed = redColor + threshold;
+
+    cv::namedWindow("Threshold img");
+    cv::moveWindow("Threshold img", 450, 50);
+    cv::namedWindow("red img");
+    cv::moveWindow("red img", 50, 450);
+    usleep(1000);
+    bool running = true;
+    while(running == true)
+    {
+        cv::Mat img;
+        camera >> img;
+        
+        clock_t startTime = clock();
+
+        //Colortracking
+        ColorTracker ct;
+        ct.setImage(img);
+        ct.generateBinary(minColor, maxColor, true);
+
+        ColorTracker rt;
+        rt.setImage(img);
+        rt.generateBinary(minRed, maxRed, true);
+
+
+        //Getting the blob data from the trackers
+        ct.generateBlobs();
+//        std::vector< Flooder::Blob > blobs = ct.getBlobs();
+        
+        clock_t endTime = clock();
+
+        std::cout << "Search took: " << (float)(endTime-startTime)/CLOCKS_PER_SEC << std::endl;
+//        std::cout << "Found: " << blobs.size() << " blobs" << std::endl;
+
+        cv::imshow("Threshold img", ct.getBinary());
+        cv::imshow("Display Image", img);
+        cv::imshow("red img", rt.getBinary());
+
+        cv::waitKey(10);
+    }
+
+    return 0;
+}
