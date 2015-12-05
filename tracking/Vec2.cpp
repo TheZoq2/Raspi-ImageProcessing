9b9924fad5c1346ee101b6568c4e5909a124b70e
Frans Skarman <frans.skarman@gmail.com>
2015-12-05 19:04:29 +0100

    Added some features to vector class

--- tracking/Vec2.cpp
+++ tracking/Vec2.cpp
@@ -1,5 +1,7 @@
 #include "Vec2.h"
 
+#include <math.h>
+
 Vec2::Vec2()
 {
     val[0] = -1;
@@ -30,22 +32,43 @@ bool Vec2::getHasValue()
     return true;
 }
 
+float Vec2::getAngle() 
+{
+    return atan2(val[1], val[0]);
+}
+float Vec2::getLength()
+{
+    return sqrt(pow(val[0], 2) + pow(val[1], 2));
+}
+
 Vec2& Vec2::operator=(Vec2 arg)
 {
     this->val[0] = arg.val[0];
     this->val[1] = arg.val[1];
     return *this;
 }
-Vec2 Vec2::operator+=(const Vec2 rhs)
+Vec2 Vec2::operator+=(const Vec2& rhs)
 {
     this->val[0] = this->val[0] + rhs.val[0];
     this->val[1] = this->val[1] + rhs.val[1];
+
+    return *this;
 }
-Vec2 Vec2::operator-=(const Vec2 rhs)
+Vec2 Vec2::operator-=(const Vec2& rhs)
 {
     this->val[0] = this->val[0] - rhs.val[0];
     this->val[1] = this->val[1] - rhs.val[1];
+
+    return *this;
 }
+Vec2 Vec2::operator*=(const float rhs)
+{
+    this->val[0] = this->val[0] * rhs;
+    this->val[1] = this->val[1] * rhs;
+
+    return *this;
+}
+
 const Vec2 Vec2::operator+(const Vec2& rhs)
 {
     Vec2 result = *this;
